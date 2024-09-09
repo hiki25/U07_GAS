ff0e5974cc52757d67f618f4c3166899c4ede43c
Jay246 <joung22462029@gmail.com>
2024-09-09 17:51:04 +0900

    0909
    WB_Credit  수정 (Clinent Credit 동기화)
    UI(mainmenu, pasue)
    
    오류 : 서버의 Credit이 안됨 -> playerState를 가져오지 못하는것으로 보임, Widget을 만들기 전에 PlayerState를 만들지 못 했다라고 판단하여 부모의 BeginPlayingState를 오버라이드 -> 블프에서 사용하는 함수 만들어 실행해보기로 함(의미가 없었음)
    HandleStartingNewPlayer의 순서 변경
    플레이어들을 먼저 받고 부모콜을 하기로 함(해결)
    
    BeginPlayingState -> 폰에게 컨트롤러가 빙의 되었을때 실행되는 함수?
    
    컨트롤러에서 PostLogin을 하면서 한 번 지우고 실행을 한다
    
    16:04 gamemode 새로운 사실
    
    인라인 텍스트
    네임 스페이, 키를 사용하여 텍스트를 번역해주는 텍스트 -> 내부에 입력되어있는 걸 사용
    
    IsInViewport - 정상적으로 Add to viewport가 되었는지

--- Source/GAS/Controller/CPlayerController.cpp
+++ Source/GAS/Controller/CPlayerController.cpp
@@ -1,10 +1,43 @@
 #include "CPlayerController.h"
+#include "Blueprint/UserWidget.h"
 
 void ACPlayerController::SetPawn(APawn* InPawn)
 {
 	Super::SetPawn(InPawn);
 
 	OnPawnChanged.Broadcast(InPawn);
+}
+
+void ACPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	InputComponent->BindAction("Escape", EInputEvent::IE_Pressed,this,&ACPlayerController::ToggleGameMenu);
+}
+
+void ACPlayerController::ToggleGameMenu()
+{
+	if (GameWidget && GameWidget->IsInViewport())
+	{
+		GameWidget->RemoveFromParent();
+		GameWidget = nullptr;
+
+		this->bShowMouseCursor = false;
+		SetInputMode(FInputModeGameOnly());
+		return;
+	}
+	GameWidget = CreateWidget<UUserWidget>(this, GameWidgetClass);
+	if (GameWidget)
+	{
+		GameWidget->AddToViewport(100);
+		this->bShowMouseCursor = true;
+		SetInputMode(FInputModeUIOnly());
+	}
+}
+
+void ACPlayerController::BeginPlayingState()
+{
+	BlueprintBegindState();
 }
 
 void ACPlayerController::OnRep_PlayerState()
@@ -14,3 +47,5 @@ void ACPlayerController::OnRep_PlayerState()
 	OnPlayerStateChanged.Broadcast(PlayerState);
 	//Todo. WB_Credit에서 변경된 PS(NULL) 인지 아닌지를 잘 받아오는지 확인
 }
+
+
